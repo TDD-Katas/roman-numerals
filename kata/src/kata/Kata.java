2b9d24e1c4604e2adfffdb44d0cf8a07f57ccda4
Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
2013-07-23 03:40:09 +0300

    First commit

---
+++ kata/src/kata/Kata.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata;
+
+/**
+ *
+ * @author Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
+ */
+public class Kata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
