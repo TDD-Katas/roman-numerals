01632968c551a29293cfeb4e99ad4696481a4add
Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
2013-07-25 16:26:46 +0300

    Introduced a constant

--- kata/src/kata/RomanSymbol.java
+++ kata/src/kata/RomanSymbol.java
@@ -9,12 +9,16 @@ package kata;
  * @author Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
  */
 class RomanSymbol {
+    //Constants
+    private static final String NO_SUBSTRACTION = "";
+    
+    //Instance vars
     private final String symbol;
     private final String substractionSymbol;
     private final int value;
 
     public RomanSymbol(String symbol, int value) {
-        this(symbol, "", value);
+        this(symbol, NO_SUBSTRACTION, value);
     }
 
     public RomanSymbol(String symbol, String substractionSymbol, int value) {
