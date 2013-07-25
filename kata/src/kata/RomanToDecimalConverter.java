eb5b1cef1aaa7ed3526046226cc186fef42b3a21
Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
2013-07-25 17:04:20 +0300

    Improve readability

--- kata/src/kata/RomanToDecimalConverter.java
+++ kata/src/kata/RomanToDecimalConverter.java
@@ -9,7 +9,8 @@ package kata;
  * @author Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
  */
 public class RomanToDecimalConverter {
-    private static final RomanSymbol[] SYMBOL_VALUE_ORDER = new RomanSymbol[] {
+
+    private static final RomanSymbol[] SYMBOL_VALUE_ORDER = new RomanSymbol[]{
         new RomanSymbol("M", "C", 1000),
         new RomanSymbol("D", "C", 500),
         new RomanSymbol("C", "X", 100),
@@ -21,20 +22,21 @@ public class RomanToDecimalConverter {
 
     /**
      * Convert
+     *
      * @param romanNumeral
-     * @return 
+     * @return
      */
     public int computeValueOf(String romanNumeral) throws InvalidRomanNumeralException {
         if (romanNumeral.length() == 0) {
             return 0;
         }
-        
+
         //Convert
         RomanSymbol dominantSymbol = identifyDominatingSymbol(romanNumeral);
         return computeValueDominatedBy(dominantSymbol, romanNumeral);
-    }    
-    
-    private RomanSymbol identifyDominatingSymbol(String romanNumeral) 
+    }
+
+    private RomanSymbol identifyDominatingSymbol(String romanNumeral)
             throws InvalidRomanNumeralException {
         RomanSymbol dominantSymbol = null;
         for (RomanSymbol romanSymbol : SYMBOL_VALUE_ORDER) {
@@ -43,43 +45,37 @@ public class RomanToDecimalConverter {
                 break;
             }
         }
-        
+
         //Test for exception
         if (dominantSymbol == null) {
             throw new InvalidRomanNumeralException();
         }
-        
+
         return dominantSymbol;
     }
 
-    private int computeValueDominatedBy(RomanSymbol symbol, String romanNumeral) 
+    private int computeValueDominatedBy(RomanSymbol symbol, String romanNumeral)
             throws InvalidRomanNumeralException {
         int indexOfSymbol = getLocationOfSymbol(romanNumeral, symbol);
-        String frontNumeral = getRomanNumeralBeforeLocation( romanNumeral, indexOfSymbol);
-        String tailNumeral = getRomanNumeralAfterLocation(romanNumeral, indexOfSymbol);
+        String frontNumeral = getNumeralBefore(indexOfSymbol, romanNumeral);
+        String tailNumeral = getNumeralAfter(indexOfSymbol, romanNumeral);
         return symbol.getValue()
                 - computeValueOf(frontNumeral)
                 + computeValueOf(tailNumeral);
-    }    
+    }
 
-    private int getLocationOfSymbol(
-            String romanNumeral, 
+    private int getLocationOfSymbol(String romanNumeral,
             RomanSymbol dominantSymbol) {
-        int locationOfDominantSymbol = romanNumeral.indexOf(dominantSymbol.getSymbol());
+        int locationOfDominantSymbol = romanNumeral
+                .indexOf(dominantSymbol.getSymbol());
         return locationOfDominantSymbol;
     }
 
-    private String getRomanNumeralBeforeLocation(
-            String romanNumeral, 
-            int locationOfDominantSymbol) {
-        String frontNumeral = romanNumeral.substring(0, locationOfDominantSymbol);
-        return frontNumeral;
+    private String getNumeralBefore(int position, String romanNumeral) {
+        return romanNumeral.substring(0, position);
     }
 
-    private String getRomanNumeralAfterLocation(
-            String romanNumeral, 
-            int locationOfDominantSymbol) {
-        String tailNumeral = romanNumeral.substring(locationOfDominantSymbol + 1);
-        return tailNumeral;
+    private String getNumeralAfter(int position, String romanNumeral) {
+        return romanNumeral.substring(position + 1);
     }
 }
