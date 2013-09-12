6c84581cae0726ccc21e00a987e6be0b70b24e1f
Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
2013-09-12 19:15:21 +0300

    Refactored tests

--- KataBowling/test/katabowling/OneFrameGameTest.java
+++ KataBowling/test/katabowling/OneFrameGameTest.java
@@ -8,45 +8,36 @@ import static org.hamcrest.core.IsEqual.*;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-
 /**
  *
  * @author Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
  */
 public class OneFrameGameTest {
-    
+
     @Test
     public void testTwoZerosScoreZero() {
-        int [] rolls = new int[] {0,0};
-        int expectedScore = 0;
-        
-        int score = computeScore(rolls);
-        
-        assertThat(score, equalTo(expectedScore));
+        assertScoreEquals(0, new int[]{0, 0});
     }
-    
+
     @Test
     public void testOneAndZeroScoreOne() {
-        int [] rolls = new int[] {1,0};
-        int expectedScore = 1;
-        
-        int score = computeScore(rolls);
-        
-        assertThat(score, equalTo(expectedScore));
+        assertScoreEquals(1, new int[]{1, 0});
     }
-    
+
     @Test
     public void testTwoAndZeroScoreTwo() {
-        int [] rolls = new int[] {2,0};
-        int expectedScore = 2;
-        
+        assertScoreEquals(2, new int[]{2, 0});
+    }
+
+    //~~~~~~~~~~~~~~~~ Test helpers ~~~~~~~~~~~
+    
+    private void assertScoreEquals(int expectedScore, int[] rolls) {
         int score = computeScore(rolls);
-        
+
         assertThat(score, equalTo(expectedScore));
     }
-    
+
     //~~~~~~~~~~~~~~~~ Production code ~~~~~~~~~~~
-    
     private int computeScore(int[] rolls) {
         return rolls[0];
     }
