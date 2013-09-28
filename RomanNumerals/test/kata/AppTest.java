e02f58d6d7a070baae57bfad6ba62307a62e051c
Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
2013-09-28 16:42:48 +0300

    First commit

---
+++ RomanNumerals/test/kata/AppTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Ignore;
+
+/**
+ *
+ * @author Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
+ */
+public class AppTest {
+    
+    @Ignore
+    @Test
+    public void main_does() {
+        fail("The test case is a prototype.");
+    }
+}
