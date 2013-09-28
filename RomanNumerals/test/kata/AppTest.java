6776c3c1074758f8bebdce92bf1fe15fc98f790d
Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
2013-09-28 17:08:30 +0300

    Refactored method to have a single exit point

--- RomanNumerals/test/kata/AppTest.java
+++ RomanNumerals/test/kata/AppTest.java
@@ -13,66 +13,61 @@ import static org.junit.Assert.*;
  * @author Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
  */
 public class AppTest {
-    
+
     @Test
     public void value_of_I_is_1() {
         assertThat(valueOf("I"), is(1));
     }
-    
+
     @Test
     public void value_of_V_is_5() {
         assertThat(valueOf("V"), is(5));
     }
-    
+
     @Test
     public void value_of_X_is_10() {
         assertThat(valueOf("X"), is(10));
     }
-    
+
     @Test
     public void value_of_L_is_50() {
         assertThat(valueOf("L"), is(50));
     }
-    
+
     @Test
     public void value_of_C_is_100() {
         assertThat(valueOf("C"), is(100));
     }
-    
+
     @Test
     public void value_of_D_is_500() {
         assertThat(valueOf("D"), is(500));
     }
-    
+
     @Test
     public void value_of_M_is_1000() {
         assertThat(valueOf("M"), is(1000));
     }
 
     protected int valueOf(String symbol) {
+        int symbolValue = 0;
         if ("I".equals(symbol)) {
-            return 1;
-        } else 
-        if ("V".equals(symbol)) {
-            return 5;
-        } else 
-        if ("X".equals(symbol)) {
-            return 10;
-        } else 
-        if ("L".equals(symbol)) {
-            return 50;
-        } else 
-        if ("C".equals(symbol)) {
-            return 100;
-        } else 
-        if ("D".equals(symbol)) {
-            return 500;
-        } else 
-        if ("M".equals(symbol)) {
-            return 1000;
+            symbolValue = 1;
+        } else if ("V".equals(symbol)) {
+            symbolValue = 5;
+        } else if ("X".equals(symbol)) {
+            symbolValue = 10;
+        } else if ("L".equals(symbol)) {
+            symbolValue = 50;
+        } else if ("C".equals(symbol)) {
+            symbolValue = 100;
+        } else if ("D".equals(symbol)) {
+            symbolValue = 500;
+        } else if ("M".equals(symbol)) {
+            symbolValue = 1000;
         }
-        
-        
-        return 1;
+
+
+        return symbolValue;
     }
 }
