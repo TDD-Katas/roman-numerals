3bc701cc4ea8fcdceaeca7938dca6de6a664105f
Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
2013-10-07 23:25:52 +0300

    Added test utility

---
+++ kata/test/kata/TestUtils.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata;
+
+/**
+ *
+ * @author Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
+ */
+public class TestUtils {
+    public static int sumValues(int[] frameScores) {
+        int scoreOfGame = 0;
+        for (int i : frameScores) {
+            scoreOfGame += i;
+        }
+        return scoreOfGame;
+    }
+}
