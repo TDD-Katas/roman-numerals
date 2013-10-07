8fd8bd0b147d133377f130b99eae79a2d13e47dd
Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
2013-10-07 20:01:24 +0300

    First commit

---
+++ kata/test/kata/KataTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
+ */
+public class KataTest {
+    
+    /**
+     * Test of main method, of class Kata.
+     */
+    @Test
+    public void testMain() {
+        fail("The test case is a prototype.");
+    }
+}
