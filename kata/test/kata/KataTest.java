52830620c973ae0a878b90ca09e019481a5b6757
Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
2013-10-14 22:12:00 +0300

    Separated the tests for Frame:

--- kata/test/kata/KataTest.java
+++ kata/test/kata/KataTest.java
@@ -16,13 +16,6 @@ import static org.mockito.Mockito.*;
  */
 public class KataTest {
     private static final int SOME_VALUE = 1;
-    private Frame frame;
-    
-    
-    @Before
-    public void setUp() {
-        frame = mock(Frame.class);
-    }
     
     @Test
     public void score_of_game_equals_sum_of_frames_scores() {
@@ -38,53 +31,13 @@ public class KataTest {
         assertThat(scoreOfGame, equalTo(firstFrameScore + secondFrameScore));
     }
 
-    @Test
-    public void score_of_a_frame_equals_plain_score_plus_bonus_score() {
-        when(frame.getStandardScore()).thenReturn(SOME_VALUE);
-        when(frame.getBonusScore()).thenReturn(SOME_VALUE);
-        when(frame.getScore()).thenCallRealMethod();
-        
-        int scoreOfFrame = frame.getScore();
 
-        assertThat(scoreOfFrame, is(frame.getStandardScore() + frame.getBonusScore()));
-    }
-
-    @Test
-    public void bonus_score_of_frame_equals_sum_of_bonus_rolls() {
-        int[] bonusRolls = createSomeRolls();
-        when(frame.getBonusRolls()).thenReturn(bonusRolls);
-        when(frame.getBonusScore()).thenCallRealMethod();
-
-        int bonusScoreForSpare = frame.getBonusScore();
-
-        assertThat(bonusScoreForSpare, is(TestUtils.sumOfValues(bonusRolls)));
-    }
-
-
-
-    @Test
-    public void plain_score_of_frame_equals_sum_of_standard_rolls() {
-        int[] standardRolls = createSomeRolls();
-        when(frame.getStandardRolls()).thenReturn(standardRolls);
-        when(frame.getStandardScore()).thenCallRealMethod();
-        
-        int sumOfRolls = frame.getStandardScore();
-
-        assertThat(sumOfRolls, is(TestUtils.sumOfValues(standardRolls)));
-    }
 
     //~~~~~~~~~~~
     protected Frame createFrameWithScore(int score) {
         Frame frame1 = mock(Frame.class);
         when(frame1.getScore()).thenReturn(score);
         return frame1;
-    }
-    
-    protected int[] createSomeRolls() {
-        int[] standardRolls = new int[]{
-            SOME_VALUE, SOME_VALUE
-        };
-        return standardRolls;
     }
     
     //~~~~~~~~~~~
@@ -103,35 +56,5 @@ public class KataTest {
         }
 
         return sumOfRolls;
-    }
-
-    static class Frame {
-        private int[] standardRolls;
-        private int[] bonusRolls;
-
-        public Frame(int[] standardRolls, int[] bonusRolls) {
-            this.standardRolls = standardRolls;
-            this.bonusRolls = bonusRolls;
-        }
-        
-        public int[] getStandardRolls() {
-            return new int[0];
-        }
-        
-        public int[] getBonusRolls() {
-            return new int[0];
-        }
-        
-        public int getStandardScore() {
-            return computeSumOfRolls(getStandardRolls());
-        }
-        
-        public int getBonusScore() {
-            return computeSumOfRolls(getBonusRolls());
-        }
-        
-        public int getScore() {
-            return getStandardScore() + getBonusScore();
-        }
     }
 }
