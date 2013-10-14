8986caa65c3ec2201766fe8649cd9708db489f4e
Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
2013-10-14 22:26:17 +0300

    Method for next frame rolls

--- kata/test/kata/KataTest.java
+++ kata/test/kata/KataTest.java
@@ -34,17 +34,33 @@ public class KataTest {
     @Test
     public void standard_rolls_for_frame_are_the_rolls_equals_to_frame_type() {
         int[] rolls = new int[] {1, 2, 3};
-        int rollsForType = 2;
+        int standardRollsNumber = 2;
         
         int[] expectedRolls = new int[] {
             1, 2
         };
         
-        int[] standardFrameRolls = new int[rollsForType];
+        int[] standardFrameRolls = new int[standardRollsNumber];
         standardFrameRolls[0] = rolls[0];
         standardFrameRolls[1] = rolls[1];
         
         assertArrayEquals(expectedRolls, standardFrameRolls);
+    }
+    
+    @Test
+    public void next_frame_rolls_for_frame_are_the_rolls_after_standard_rolls() {
+        int[] rolls = new int[] {1, 2, 3, 4};
+        int standardRollsNumber = 2;
+        
+        int[] expectedRolls = new int[] {
+            3, 4
+        };
+        
+        int[] nextFrameRolls = new int[rolls.length - standardRollsNumber];
+        nextFrameRolls[0] = rolls[standardRollsNumber];
+        nextFrameRolls[1] = rolls[standardRollsNumber + 1];
+        
+        assertArrayEquals(expectedRolls, nextFrameRolls);
     }
     
     //~~~~~~~~~~~
