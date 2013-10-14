dbf031d9535a4f0b357e2700142a0ef4276eefee
Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
2013-10-14 22:06:41 +0300

    Constant for frame mock

--- kata/test/kata/KataTest.java
+++ kata/test/kata/KataTest.java
@@ -7,6 +7,7 @@ package kata;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+import org.junit.Before;
 import static org.mockito.Mockito.*;
 
 /**
@@ -15,8 +16,13 @@ import static org.mockito.Mockito.*;
  */
 public class KataTest {
     private static final int SOME_VALUE = 1;
-
+    private Frame frameInstance;
     
+    
+    @Before
+    public void setUp() {
+        frameInstance = mock(Frame.class);
+    }
     
     @Test
     public void score_of_game_equals_sum_of_frames_scores() {
@@ -36,12 +42,11 @@ public class KataTest {
     public void score_of_a_frame_equals_plain_score_plus_bonus_score() {
         int plainScore = SOME_VALUE;
         int bonusScore = SOME_VALUE;
-        Frame frame = mock(Frame.class);
-        when(frame.getPlainScore()).thenReturn(plainScore);
-        when(frame.getBonusScore()).thenReturn(bonusScore);
-        when(frame.getScore()).thenCallRealMethod();
+        when(frameInstance.getPlainScore()).thenReturn(plainScore);
+        when(frameInstance.getBonusScore()).thenReturn(bonusScore);
+        when(frameInstance.getScore()).thenCallRealMethod();
         
-        int scoreOfFrame = frame.getScore();
+        int scoreOfFrame = frameInstance.getScore();
 
         assertThat(scoreOfFrame, is(plainScore + bonusScore));
     }
@@ -49,11 +54,10 @@ public class KataTest {
     @Test
     public void bonus_score_of_frame_equals_sum_of_bonus_rolls() {
         int[] bonusRolls = createSomeRolls();
-        Frame frame = mock(Frame.class);
-        when(frame.getBonusRolls()).thenReturn(bonusRolls);
-        when(frame.getBonusScore()).thenCallRealMethod();
+        when(frameInstance.getBonusRolls()).thenReturn(bonusRolls);
+        when(frameInstance.getBonusScore()).thenCallRealMethod();
 
-        int bonusScoreForSpare = frame.getBonusScore();
+        int bonusScoreForSpare = frameInstance.getBonusScore();
 
         assertThat(bonusScoreForSpare, is(TestUtils.sumValues(bonusRolls)));
     }
@@ -63,11 +67,10 @@ public class KataTest {
     @Test
     public void plain_score_of_frame_equals_sum_of_standard_rolls() {
         int[] standardRolls = createSomeRolls();
-        Frame frame = mock(Frame.class);
-        when(frame.getFrameRolls()).thenReturn(standardRolls);
-        when(frame.getPlainScore()).thenCallRealMethod();
+        when(frameInstance.getFrameRolls()).thenReturn(standardRolls);
+        when(frameInstance.getPlainScore()).thenCallRealMethod();
         
-        int sumOfRolls = frame.getPlainScore();
+        int sumOfRolls = frameInstance.getPlainScore();
 
         assertThat(sumOfRolls, is(TestUtils.sumValues(standardRolls)));
     }
