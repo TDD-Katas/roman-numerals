ab444fe1004f40cffe2e762c0811d69532a2a45b
Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
2013-10-24 00:55:34 +0300

    Used mocks for symbol

--- kata/src/test/java/ro/ghionoiu/kata/AppTest.java
+++ kata/src/test/java/ro/ghionoiu/kata/AppTest.java
@@ -14,14 +14,21 @@ import static org.mockito.Mockito.*;
  * @author Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
  */
 public class AppTest {
+    public static final int SOME_VALUE = 1;
     
     @Test
     public void value_of_numeral_with_one_symbol_is_that_symbols_value() {
-        Symbol symbol = new Symbol("X");
+        Symbol symbol = getSomeSymbol();
         Numeral romanNumeral = new Numeral(symbol);
         
         final int valueOfRomanNumeral = romanNumeral.getValue();
         
         assertThat(valueOfRomanNumeral, is(symbol.getValue()));
+    }
+
+    protected Symbol getSomeSymbol() {
+        Symbol symbol = mock(Symbol.class);
+        when(symbol.getValue()).thenReturn(SOME_VALUE);
+        return symbol;
     }
 }
