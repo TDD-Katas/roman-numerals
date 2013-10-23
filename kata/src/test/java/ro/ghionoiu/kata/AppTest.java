7660e9c0024304d8c49d1fe04e006ddf09127ba6
Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
2013-10-23 23:53:13 +0300

    Initial commit - acceptance tests in place

---
+++ kata/src/test/java/ro/ghionoiu/kata/AppTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ro.ghionoiu.kata;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+/**
+ *
+ * @author Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
+ */
+public class AppTest {
+    @Test
+    public void testMain() {
+        assertThat(true, is(true));
+    }
+}
---
+++ kata/src/test/specs/ro/ghionoiu/kata/RomanNumeralFixture.java
@@ -0,0 +1,12 @@
+package ro.ghionoiu.kata;
+
+import org.concordion.integration.junit4.ConcordionRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(ConcordionRunner.class)
+public class RomanNumeralFixture {
+
+	public int romanToDecimal(String romanNumber) {
+		return 0;
+	}
+}
