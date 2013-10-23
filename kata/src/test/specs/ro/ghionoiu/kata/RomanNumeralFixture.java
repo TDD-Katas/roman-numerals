5a8af2c7ca700444103e35cb5d8e92c576edd30f
Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
2013-10-24 00:56:45 +0300

    Passed 5 acceptance tests

--- kata/src/test/specs/ro/ghionoiu/kata/RomanNumeralFixture.java
+++ kata/src/test/specs/ro/ghionoiu/kata/RomanNumeralFixture.java
@@ -6,7 +6,9 @@ import org.junit.runner.RunWith;
 @RunWith(ConcordionRunner.class)
 public class RomanNumeralFixture {
 
-	public int romanToDecimal(String romanNumber) {
-		return 0;
-	}
+    public int romanToDecimal(String romanNumber) {
+        Numeral numeral = new Numeral(new Symbol(romanNumber));
+
+        return numeral.getValue();
+    }
 }
