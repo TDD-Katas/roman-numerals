afe28059ff2871c4d9777c49c9895c2ff241fb10
Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
2013-10-24 01:20:23 +0300

    Used chars in symbols tests

--- kata/src/test/java/ro/ghionoiu/kata/SymbolTest.java
+++ kata/src/test/java/ro/ghionoiu/kata/SymbolTest.java
@@ -16,41 +16,42 @@ public class SymbolTest {
     
     @Test
     public void value_of_I_is_1() {
-        assertCharacterValueIs(1, "I");
+        assertCharacterValueIs(1, 'I');
     }
     
     @Test
     public void value_of_V_is_5() {
-        assertCharacterValueIs(5, "V");
+        assertCharacterValueIs(5, 'V');
     }
     
     @Test
     public void value_of_X_is_10() {
-        assertCharacterValueIs(10, "X");
+        assertCharacterValueIs(10, 'X');
     }
     
     @Test
     public void value_of_L_is_50() {
-        assertCharacterValueIs(50, "L");
+        assertCharacterValueIs(50, 'L');
     }
     
     @Test
     public void value_of_C_is_100() {
-        assertCharacterValueIs(100, "C");
+        assertCharacterValueIs(100, 'C');
     }
     
     @Test
     public void value_of_D_is_500() {
-        assertCharacterValueIs(500, "D");
+        assertCharacterValueIs(500, 'D');
     }
     
     @Test
     public void value_of_D_is_1000() {
-        assertCharacterValueIs(1000, "M");
+        assertCharacterValueIs(1000, 'M');
     }
 
     
-    protected void assertCharacterValueIs(int value, String string) {
-        assertThat(new Symbol(string).getValue(), equalTo(value));
+    protected void assertCharacterValueIs(int value, char character) {
+        String number = character + "";
+        assertThat(new Symbol(number).getValue(), equalTo(value));
     }
 }
