9d0c5960084683bd588d5e65ed10b72546598e30
Iulian Ghionoiu <iulian.ghionoiu@exenne.ro>
2013-10-25 12:23:26 +0300

    Renamed method

--- kata/src/test/java/ro/ghionoiu/kata/AppTest.java
+++ kata/src/test/java/ro/ghionoiu/kata/AppTest.java
@@ -32,7 +32,7 @@ public class AppTest {
         int currentValue = 1;
         int nextValue = 10;
         
-        Sign sign = getSignForValue(currentValue, nextValue);
+        Sign sign = getSignForCurrentValue(currentValue, nextValue);
         
         assertThat(sign, is(Sign.MINUS));
     }
@@ -42,7 +42,7 @@ public class AppTest {
         int currentValue = 10;
         int nextValue = 5;
         
-        Sign sign = getSignForValue(currentValue, nextValue);
+        Sign sign = getSignForCurrentValue(currentValue, nextValue);
         
         assertThat(sign, is(Sign.PLUS));
     }
@@ -52,7 +52,7 @@ public class AppTest {
         int currentValue = 10;
         int nextValue = 10;
         
-        Sign sign = getSignForValue(currentValue, nextValue);
+        Sign sign = getSignForCurrentValue(currentValue, nextValue);
         
         assertThat(sign, is(Sign.PLUS));
     }
@@ -118,15 +118,7 @@ public class AppTest {
         
     }
     
-    protected int adjust(Sign sign, int currentValue) {
-        int adjustedValue = 0;
-        
-        adjustedValue = sign.adjust(currentValue);
-        
-        return adjustedValue;
-    }
-    
-    protected Sign getSignForValue(int value, int nextValue) {
+    protected Sign getSignForCurrentValue(int value, int nextValue) {
         Sign operation = Sign.PLUS;
         
         if (value < nextValue) {
