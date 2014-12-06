5989d0fee3003811e973e2a1ecf5c10ddf118de4
Julian Ghionoiu <julian.ghionoiu@valtech.co.uk>
2014-12-06 15:06:39 +0000

    Small refactoring

--- v4_acceptance_with_priority_premise/kata/src/test/java/AppTest.java
+++ v4_acceptance_with_priority_premise/kata/src/test/java/AppTest.java
@@ -6,35 +6,41 @@ import static org.junit.Assert.*;
 public class AppTest {
 
     @Test
-    public void value_of_empty_is_zero() {
+    public void value_of_empty_is_0() {
         assertThat(valueOf(""), is(0));
     }
 
     @Test
-    public void value_of_I_is_one() {
+    public void value_of_I_is_1() {
         assertThat(valueOf("I"), is(1));
     }
 
     @Test
-    public void value_of_V_is_five() {
+    public void value_of_V_is_5() {
         assertThat(valueOf("V"), is(5));
     }
 
     @Test
-    public void value_of_X_is_ten() {
+    public void value_of_X_is_10() {
         assertThat(valueOf("X"), is(10));
     }
+
+
+//    L	50
+//    C	100
+//    D	500
+//    M 1000
 
     //~~~ Impl
 
     private int valueOf(String numeral) {
-        if (numeral == "") {
+        if (numeral.equals("")) {
             return 0;
         }
-        if (numeral == "I") {
+        if (numeral.equals("I")) {
             return 1;
         }
-        if (numeral == "V") {
+        if (numeral.equals("V")) {
             return 5;
         }
 
