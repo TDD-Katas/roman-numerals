199580e214b4adc08aa0037f3cc99cdccb6e53f3
Julian Ghionoiu <julian.ghionoiu@valtech.co.uk>
2014-12-06 16:37:03 +0000

    Wraped the numeral primitive

--- v4_acceptance_with_priority_premise/kata/src/test/java/AppTest.java
+++ v4_acceptance_with_priority_premise/kata/src/test/java/AppTest.java
@@ -12,69 +12,88 @@ public class AppTest {
 
     @Test
     public void value_of_empty_is_0() {
-        assertThat(valueOf(""), is(0));
+        assertThat(valueOf(numeral("")), is(0));
     }
 
     @Test
     public void value_of_I_is_1() {
-        assertThat(valueOf("I"), is(1));
+        assertThat(valueOf(numeral("I")), is(1));
     }
 
     @Test
     public void value_of_V_is_5() {
-        assertThat(valueOf("V"), is(5));
+        assertThat(valueOf(numeral("V")), is(5));
     }
 
     @Test
     public void value_of_X_is_10() {
-        assertThat(valueOf("X"), is(10));
+        assertThat(valueOf(numeral("X")), is(10));
     }
 
     @Test
     public void value_of_L_is_50() {
-        assertThat(valueOf("L"), is(50));
+        assertThat(valueOf(numeral("L")), is(50));
     }
 
     @Test
     public void value_of_C_is_100() {
-        assertThat(valueOf("C"), is(100));
+        assertThat(valueOf(numeral("C")), is(100));
     }
 
     @Test
     public void value_of_D_is_500() {
-        assertThat(valueOf("D"), is(500));
+        assertThat(valueOf(numeral("D")), is(500));
     }
 
     @Test
     public void value_of_M_is_1000() {
-        assertThat(valueOf("M"), is(1000));
+        assertThat(valueOf(numeral("M")), is(1000));
     }
 
     @Test( expected = IllegalCharacterException.class )
     public void throws_exception_for_unrecognized_character() {
-        valueOf(UNRECOGNIZED_CHARACTER);
+        valueOf(numeral(UNRECOGNIZED_CHARACTER));
     }
 
     @Test
     public void value_of_two_character_numeral_is_their_sum() {
-        assertThat(valueOf("VI"), is(6));
+        assertThat(valueOf(numeral("VI")), is(6));
     }
 
     @Test
     public void value_of_three_character_numeral_is_their_sum() {
-        assertThat(valueOf("XVI"), is(16));
+        assertThat(valueOf(numeral("XVI")), is(16));
     }
 
     @Test
     public void value_of_small_literal_before_big_literal_is_substracted() {
-        assertThat(valueOf("IV"), is(4));
+        assertThat(valueOf(numeral("IV")), is(4));
     }
 
     //~~~ Impl
 
     public static class IllegalCharacterException extends RuntimeException {
+
+    }
+    private static class Numeral {
+
+        private String value;
+        private Numeral(String value) {
+            this.value = value;
+        }
+
+        public int length() {
+            return value.length();
+        }
+
+        public char charAt(int index) {
+            return value.charAt(index);
+        }
     }
 
+    private static Numeral numeral(String s) {
+        return new Numeral(s);
+    }
 
     private static final Map<Character, Integer> VALUES = new HashMap<>();
     static {
@@ -96,21 +115,19 @@ public class AppTest {
         return value;
     }
 
-    private int valueOf(String numeral) {
+    private int valueOf(Numeral numeral) {
         int sum = 0;
         for (int i = 0; i < numeral.length(); i++) {
 
             int currentValue = valueOfLiteral(numeral.charAt(i));
+            int nextValue = 0;
 
             if (i+1 < numeral.length()) {
-                int nextValue = valueOfLiteral(numeral.charAt(i + 1));
+                nextValue = valueOfLiteral(numeral.charAt(i + 1));
+            }
 
-
-                if (currentValue < nextValue) {
-                    sum -= currentValue;
-                } else {
-                    sum += currentValue;
-                }
+            if (currentValue < nextValue) {
+                sum -= currentValue;
             } else {
                 sum += currentValue;
             }
